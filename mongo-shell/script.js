2bdbdca507c82b1f19cdaab1d80856c556f6de19
rudiarta <rudiarta321@gmail.com>
2022-02-08 18:49:28 +0800

    modify

--- mongo-shell/script.js
+++ mongo-shell/script.js
@@ -5,3 +5,9 @@ db.students.getIndexes();
 db.students.dropIndex("name_text");
 
 db.students.find({$text:{$search:"lagi"}});
+
+db.customer.find({"customer_name":{$regex:/az/i}, "customer_account_type":"partner", "customer_ref_id":{$in:[9578]}}).explain("executionStats");
+// db.customer.createIndex({"customer_name":1});
+// db.customer.createIndex({"customer_account_type":1});
+// db.customer.createIndex({"customer_ref_id":1});
+db.customer.dropIndex("idx_search");
